a8cedd448fc36073999123568dfa646ac5ea861a
Aliza Pilisuk <apilisuk@gmail.com>
2016-08-22 20:41:42 -0700

    added names and changed homepage and fullsize view

--- lab-aliza/app/data/images.js
+++ lab-aliza/app/data/images.js
@@ -3,17 +3,21 @@
 module.exports =  [{
   id: 1,
   url: 'https://media.giphy.com/media/14kdiJUblbWBXy/giphy.gif',
-  description: 'Hack the planet'
+  description: 'Hack it real good.',
+  title: 'Hack the planet'
 }, {
   id: 2,
   url: 'https://media.giphy.com/media/TOWeGr70V2R1K/giphy.gif',
-  description: 'Hackers in the mainframe'
+  description: 'Oh no!',
+  title: 'Hackers in the mainframe'
 }, {
   id: 3,
   url: 'http://motherboard-images.vice.com/content-images/contentimage/no-id/1442506168700922.gif',
-  description: 'Floppy disks are cool'
+  description: 'Do not try this at home.',
+  title: 'Floppy disks are cool'
 }, {
   id: 4,
   url: 'https://arikia.files.wordpress.com/2013/02/hackers-mathew-lillard.gif',
-  description: 'Pagers are cool too'
+  description: 'Beep beep.',
+  title: 'Pagers are cool too'
 }];
