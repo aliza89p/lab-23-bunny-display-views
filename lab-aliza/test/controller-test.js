deca19e4de2e8aa0c494c14220098a8c8b68ed62
Aliza Pilisuk <apilisuk@gmail.com>
2016-08-22 20:56:59 -0700

    added test for error

--- lab-aliza/test/controller-test.js
+++ lab-aliza/test/controller-test.js
@@ -8,22 +8,33 @@ describe('testing controllers used in routing', function() {
     angular.mock.inject(($controller) => {
       this.homeController = new $controller('HomeController');
       this.fullsizeController = new $controller('FullsizeController');
+      this.thumbnailController = new $controller('ThumbnailController');
+      this.errorController = new $controller('ErrorController');
     });
   });
 
-  it('home controller should create a list of messages', () => {
+  it('home controller should create a list of images', () => {
     expect(this.homeController.images.length > 0).toEqual(true);
     expect(this.homeController.images.length).toEqual(4);
   });
 
-  it('fullsize controller should create a list of messages', () => {
+  it('fullsize controller should create a list of images', () => {
     expect(this.fullsizeController.images.length > 0).toEqual(true);
     expect(this.fullsizeController.images.length).toEqual(4);
+  });
+
+  it('thumbnail controller should create a list of images', () => {
+    expect(this.thumbnailController.images.length > 0).toEqual(true);
+    expect(this.thumbnailController.images.length).toEqual(4);
   });
 
   it('fullsize should be able to test for a valid id', () => {
     expect(this.fullsizeController.isValidId('x')).toEqual(false);
     expect(this.fullsizeController.isValidId(1)).toEqual(true);
+  });
+
+  it('invalid route should send error', () => {
+    expect(this.errorController.errorMessage).toBe('Error trying to access unknown path');
   });
 
 });
